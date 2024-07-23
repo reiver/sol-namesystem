f31e5be291eeb03fd3b5c3d6e14e167c149bf0a7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-07-23 05:28:59 -0700

    script/namesystem.s.sol

---
+++ script/namesystem.s.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {NameSystem} from "../src/namesystem.sol";
+
+contract NameSystemScript is Script {
+    NameSystem public namesystem;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        namesystem = new NameSystem("dorood");
+
+        vm.stopBroadcast();
+    }
+}
