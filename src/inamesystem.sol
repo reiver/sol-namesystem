20cf52f7e792f2c656728f80b8210b46e86183b3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-07-22 20:39:14 -0700

    INameSystem

--- src/inamesystem.sol
+++ src/inamesystem.sol
@@ -4,6 +4,6 @@ interface INameSystem {
 	function get(bytes32 digest) external view returns(address);
 	function length() external view returns (uint256);
 	function namespace() external view returns(string memory);
-	function owner() external view returns(address);
+	function potentate() external view returns(address);
 	function set(bytes32 digest, address value) external;
 }
